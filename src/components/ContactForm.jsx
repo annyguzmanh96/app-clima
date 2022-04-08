9ad6a735177dd085c60fd52a219828c17e04329b
annyguzmanh96 <anamariaguzman1996@hotmail.com>
2022-04-08 14:21:19 -0500

    App-Clima Ana

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,30 @@
+import style from './ContactForm.module.css';
+import { BiMailSend } from 'react-icons/bi';
+import { GrLocation } from 'react-icons/gr';
+import { AiOutlinePhone } from 'react-icons/ai';
 
 export function ContactForm(){
     return(
-        <div>
-            <h1>HOLA, AQUI VA UN FORM</h1>
+        <div className={style.containerAll}>
+            <div className={style.DatosContacto}>
+                <h5><BiMailSend />contacto@app-clima.com.co</h5>
+                <h5><GrLocation />Calle 3b No.3b-311</h5>
+                <h5>< AiOutlinePhone />3005922807</h5>
+               
+
+            </div>
+            <form   className={style.form}>
+                <h1>Contacto</h1>
+                <label><h1>Nombre</h1></label>
+                &nbsp;<input></input>
+                <label><h1>Correo Electrónico</h1></label>
+                &nbsp;<input></input>
+                <label><h1>Asunto</h1></label>
+                &nbsp;<input></input>
+                <label><h1>Mensaje</h1></label>
+                &nbsp;<input></input>
+                
+            </form>
         </div>
     )
 }
