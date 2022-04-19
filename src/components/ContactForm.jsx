34354e8ee47dd7cb5dd6c9858029b3e6185196c6
annyguzmanh96 <anamariaguzman1996@hotmail.com>
2022-04-19 12:30:50 -0500

    App Clima Ana

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,25 +6,25 @@ import { AiOutlinePhone } from 'react-icons/ai';
 export function ContactForm(){
     return(
         <div className={style.containerAll}>
-            <div className={style.DatosContacto}>
-                <h5><BiMailSend />contacto@app-clima.com.co</h5>
-                <h5><GrLocation />Calle 3b No.3b-311</h5>
-                <h5>< AiOutlinePhone />3005922807</h5>
-               
+            <div className={style.container}>
+                <div className={style.DatosContacto}>
+                    <h5><BiMailSend />contacto@app-clima.com.co</h5>
+                    <h5><GrLocation />Calle 3b No.3b-311</h5>
+                    <h5>< AiOutlinePhone />3005922807</h5>
+                </div>
+                <form   className={style.form}>
+                    <p className={style.formTitle}>Contacto</p>
+                    <label className={style.label}>Nombre</label>
+                    <input placeholder='Escriba su nombre aquí'/>&nbsp;
+                    <label className={style.label}>Correo Electrónico</label>
+                    <input placeholder='Escriba su correo aquí'/>&nbsp;
+                    <label className={style.label}>Asunto</label>
+                    <input placeholder='Escriba el asunto aquí'/>&nbsp;
+                    <label className={style.label}>Mensaje</label>
+                    <input placeholder='Escriba el mensaje aquí...' />&nbsp;
 
+                </form>
             </div>
-            <form   className={style.form}>
-                <h1>Contacto</h1>
-                <label><h1>Nombre</h1></label>
-                &nbsp;<input></input>
-                <label><h1>Correo Electrónico</h1></label>
-                &nbsp;<input></input>
-                <label><h1>Asunto</h1></label>
-                &nbsp;<input></input>
-                <label><h1>Mensaje</h1></label>
-                &nbsp;<input></input>
-                
-            </form>
         </div>
     )
 }
